ffe6d861ca7356a82f8b7e50e6a9df3918f48d49
Muhammad Roisul Adib <99470918+roisuladib@users.noreply.github.com>
2024-07-19 20:36:49 +0700

    add: middleware file

---
+++ src/middleware.ts
@@ -0,0 +1,22 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+const isDevelopment = process.env.NODE_ENV !== 'production';
+
+export function middleware(req: NextRequest) {
+   // retrieve the current response
+   const res = NextResponse.next();
+
+   // add the CORS headers to the response
+   res.headers.append('Access-Control-Allow-Credentials', 'true');
+   res.headers.append('Access-Control-Allow-Origin', isDevelopment ? '*' : req.nextUrl.origin);
+   res.headers.append('Access-Control-Allow-Methods', 'GET');
+   res.headers.append('Cross-Origin-Opener-Policy', 'same-origin');
+   res.headers.append('Referrer-Policy', 'strict-origin-when-cross-origin');
+
+   return res;
+}
+
+// specify the path regex to apply the middleware to
+export const config = {
+   matcher: '/api/:path*',
+};
