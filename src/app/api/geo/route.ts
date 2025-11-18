68079a57951491139bd576495b6d2bf0336c6acb
Muhammad Roisul Adib <adib17r@gmail.com>
2025-11-18 17:06:25 +0700

    change: geo2

--- src/app/api/geo/route.ts
+++ src/app/api/geo/route.ts
@@ -6,5 +6,6 @@ import { getCurrency } from 'locale-currency';
 export async function GET(request: NextRequest) {
   const headers = request.headers;
   const details = geolocation(request);
-  return Response.json({ ...details, headers, id: getCurrency(details.country || 'US') });
+  const countryCode = headers.get('x-vercel-ip-country') || 'US'; // Default to 'US' if header is not present
+  return Response.json({ ...details, headers, id: getCurrency(details.country || 'US'), countryCode });
 }
