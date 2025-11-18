3920ce89fc1ff20d487627d0133dd808852846a4
Muhammad Roisul Adib <adib17r@gmail.com>
2025-11-18 16:27:25 +0700

    change: geo

--- src/app/api/geo/route.ts
+++ src/app/api/geo/route.ts
@@ -1,9 +1,10 @@
 import type { NextRequest } from 'next/server';
 
 import { geolocation } from '@vercel/functions';
+import { getCurrency } from 'locale-currency';
 
 export async function GET(request: NextRequest) {
-  const headers = request.headers
+  const headers = request.headers;
   const details = geolocation(request);
-  return Response.json({ ...details, headers })
+  return Response.json({ ...details, headers, id: getCurrency(details.country || 'US') });
 }
