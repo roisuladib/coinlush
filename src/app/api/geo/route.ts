4abd77f7e3878009af5aeaa2c0fe9bd51aa9d907
Muhammad Roisul Adib <adib17r@gmail.com>
2025-11-18 15:54:25 +0700

    change

--- src/app/api/geo/route.ts
+++ src/app/api/geo/route.ts
@@ -3,6 +3,7 @@ import type { NextRequest } from 'next/server';
 import { geolocation } from '@vercel/functions';
 
 export async function GET(request: NextRequest) {
+  const headers = request.headers
   const details = geolocation(request);
-  return Response.json(details)
+  return Response.json({ ...details, headers })
 }
