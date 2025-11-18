e4a34606a39d8727739ee2a67fcffd055002b093
Muhammad Roisul Adib <adib17r@gmail.com>
2025-11-18 17:13:08 +0700

    fix: country

--- src/app/api/geo/route.ts
+++ src/app/api/geo/route.ts
@@ -1,11 +1,9 @@
 import type { NextRequest } from 'next/server';
 
-import { geolocation } from '@vercel/functions';
 import { getCurrency } from 'locale-currency';
 
 export async function GET(request: NextRequest) {
   const headers = request.headers;
-  const details = geolocation(request);
-  const countryCode = headers.get('x-vercel-ip-country') || 'US'; // Default to 'US' if header is not present
-  return Response.json({ ...details, headers, id: getCurrency(details.country || 'US'), countryCode });
+  const local = headers.get('x-vercel-ip-country') || 'US';
+  return Response.json({ countryCode: getCurrency(local) });
 }
