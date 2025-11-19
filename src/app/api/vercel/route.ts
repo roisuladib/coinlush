395b45e1bc0c95a4be4f0d3bc38ea6c3176d8e26
Muhammad Roisul Adib <adib17r@gmail.com>
2025-11-19 15:25:04 +0700

    remove: @vercel/functions

--- src/app/api/vercel/route.ts
+++
@@ -1,9 +0,0 @@
-import type { NextRequest } from 'next/server';
-
-import { NextResponse } from 'next/server';
-
-import { geolocation } from '@vercel/functions';
-
-export async function GET(request: NextRequest) {
-  return NextResponse.json(geolocation(request));
-}
